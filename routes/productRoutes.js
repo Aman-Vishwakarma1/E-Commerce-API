46c7e52290e79fc69b84e3dbc36225da00abe5f4
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2024-12-30 00:08:47 +0530

    remove product route file

--- routes/productRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const validateToken = require("../middleware/tokenValidationHandler");
-const { addProduct } = require("../controllers/productController");
-router.use(validateToken);
-
-module.exports = router;
