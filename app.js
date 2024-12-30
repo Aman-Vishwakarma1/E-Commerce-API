fc34a592677d51bc73df9d5ca07e18568e927cf5
Aman Vishwakarma <me.amanvishwakarma@gmail.com>
2024-12-31 00:56:27 +0530

    swagger added

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const path = require("path");
+const swaggerUI = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 
 // Database Connection
 const connectDb = require("./config/dbconnect");
@@ -14,6 +17,15 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "views")));
 app.set("view engine", "ejs");
 connectDb();
+
+// const corsOptions = {
+//   origin: "http://localhost:5500", // Allow only requests from this origin
+//   methods: "GET,POST", // Allow only these methods
+//   allowedHeaders: ["Content-Type", "Authorization"], // Allow only these headers
+// };
+
+app.use(cors());
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/home", require("./routes/homeRoutes.js")); // Home Routes
 app.use("/seller", require("./routes/sellerRoutes.js")); // Owner Routes
